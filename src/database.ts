f67ba872fa75bed4fee48a5314cb8d2ae5d9299e
Maxence Frenette <maxence.frenette@gmail.com>
2017-12-03 16:48:53 -0500

    Added database connection

---
+++ src/database.ts
@@ -0,0 +1,82 @@
+import { DynamoDB, config } from 'aws-sdk';
+import { PutItemInput, PutItemInputAttributeMap, GetItemInput, AttributeMap } from 'aws-sdk/clients/dynamodb';
+import { resolve } from 'url';
+
+const TABLE_NAME = 'chess-opening-table';
+
+export class Position {
+    constructor(
+        public fen: string,
+        public whiteWin: number,
+        public blackWin: number,
+        public children: number,
+    ) {}
+
+    toItemAttributeMap(): PutItemInputAttributeMap {
+        return {
+            fen: { S: this.fen },
+            whiteWin: { N: this.whiteWin.toString() },
+            blackWin: { N: this.blackWin.toString() },
+            children: { N: this.children.toString() },
+        }
+    }
+
+    static fromAttributeMap(map: AttributeMap) {
+        // TODO: null error handling
+        return new Position(
+            map.fen.S!,
+            parseInt(map.whiteWin.N!),
+            parseInt(map.blackWin.N!),
+            parseInt(map.children.N!),
+        );
+    }
+}
+
+export class Database {
+    db: DynamoDB
+    
+    constructor(region: string) {
+        config.update({ region });
+        this.db = new DynamoDB();
+    }
+
+    getPosition(fen: string): Promise<Position | undefined> {
+        let params: GetItemInput = {
+            TableName: TABLE_NAME,
+            Key: {
+                'fen': { S: fen },
+            },
+        }
+
+        return new Promise((resolve, reject) => {
+            this.db.getItem(params, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (data.Item) {
+                        resolve(Position.fromAttributeMap(data.Item));
+                    } else {
+                        resolve(undefined);
+                    }
+                }
+            })
+        })
+    }
+
+    putPosition(position: Position): Promise<void> {
+        let params: PutItemInput = {
+            TableName: TABLE_NAME,
+            Item: position.toItemAttributeMap(),
+        }
+
+        return new Promise((resolve, reject) => {
+            this.db.putItem(params, (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            })
+        })
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 /// <reference path="types/node-uci.d.ts" />
 
 import { Result, playGame } from './chess';
+import { Database, Position } from './database';
 
 playGame('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', 100000).then(console.log);
+
+let db = new Database('us-west-2');
+db.putPosition(new Position('test', 1, 0, 0)).then(console.log);
+db.getPosition('test').then(console.log);
